0eab5f8dbea7c99d8ed867a7cd149c000b1c14b9
daniel-unger <danunger24@gmail.com>
2018-01-31 20:41:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+// # 1)
+  function shout(string) {
+    return string.toUpperCase()
+   }
+// # 2)
+  function logShout(string) {
+    //  return string.toUpperCase()  //so this function will 
+       console.log(string.toUpperCase()) 
+
 //# 3)
-/*  function whisper(string) {
+  function whisper(string) {
        return string.toLowerCase()
     }*/
     function whisper(string) {
          //return string.toLowerCase()
        console.log(string.toLowerCase());
       }
-
+//# 4)
       function sayHiToGrandma(string) {
    return string.toLowerCase()
    console.log(string.toLowerCase());
