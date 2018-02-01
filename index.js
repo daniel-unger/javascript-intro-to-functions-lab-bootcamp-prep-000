904d0b6f1a435c1d323034b1ca495dc417f41adc
daniel-unger <danunger24@gmail.com>
2018-02-01 01:56:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,17 +22,22 @@ function whisper(string) {
   }
 
 
-//# 4)
+//# 4)sayHiToGrandma(string) returns "I love you, too." if `string` is "I love you, Grandma."
       function sayHiToGrandma(string) {
 
-if  (string === string.toLowerCase()){
-    return  "I can't hear you!"}
+        if  (string === string.toLowerCase()){
+          return  "I can't hear you!"
+        }
    //console.log('I can\'t hear you!')
 
-  if (string === string.toUpperCase()){
-     return  "YES INDEED!"}
+   if (string === string.toUpperCase()){
+      return  "YES INDEED!"
+        }
+      else {
+        string = "I love you, Grandma.";
+      return "I love you, Grandma."
       }
-
+        }
 
   //  console.log('YES INDEED!')
     // return  "YES INDEED!"
