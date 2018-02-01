737900960c227ca473f1a1ff9bf6265fafc0e221
daniel-unger <danunger24@gmail.com>
2018-02-01 00:40:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function whisper(string) {
       function sayHiToGrandma(string) {
 
 if (string === string.toLowerCase()
-   console.log('I can\'t hear you!')
+   //console.log('I can\'t hear you!')
     return  "I can't hear you!"
 
 
